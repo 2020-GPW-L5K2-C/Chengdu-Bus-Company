77cf50fbc5f707cfaac1665dce59eb9ec09bf3fc
Ivy <48086641+IvyFeng413@users.noreply.github.com>
2020-05-17 19:21:24 +0800

    login

---
+++ login.php
@@ -0,0 +1,154 @@
+<?php
+
+// make db conection
+require('includes/db.php');
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        $error = "username or password is empty";
+    } else { 
+        // Save username & password in a variable
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // 2. Prepare query
+        $query  = "SELECT username, password, level "; 
+        $query .= "FROM users ";
+        $query .= "WHERE username = '$username' AND password = '$password' ";
+
+        // 2. Execute query
+        $result = mysqli_query($connection, $query);
+
+        if (!$result) {
+            die("query is wrong");
+        }
+
+        // Save data to $row
+        $row = mysqli_fetch_array($result);
+        
+        // Check how many answers did we get
+        $numrows=mysqli_num_rows($result);
+        if ($numrows == 1) {
+            // Start to use sessions
+            session_start();
+            
+            // Create session variables
+            $_SESSION['login_user'] = $username;
+            $_SESSION['login_level'] = $row['level'];
+            
+            if ($_SESSION['login_level'] == 1) {
+                header('location: index.html');
+            } 
+            if ($_SESSION['login_level'] == 2) {
+                header('location: index.html');
+            } 
+            else if ($_SESSION['login_level'] == 3) {
+                header('location: index.html');
+            } 
+            else  {
+                header('location: index.html');
+            }
+            
+        } 
+        else {
+            echo "Login failed";
+        }
+        
+        // 4. free results
+        mysqli_free_result($result);
+    }
+}
+
+// 5. close db connection
+mysqli_close($connection);
+
+?>
+
+<?php
+
+//require('includes/loginstyle.php');
+
+if (isset($error)) {
+    echo "<span>" . $error ."</span>";
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Custom Font Icons CSS-->
+    <link rel="stylesheet" href="css/font.css">
+    <!-- Google fonts - Muli-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <div class="login-page">
+      <div class="container d-flex align-items-center">
+        <div class="form-holder has-shadow">
+          <div class="row">
+            <!-- Logo & Information Panel-->
+            <div class="col-lg-6">
+              <div class="info d-flex align-items-center">
+                <div class="content">
+                  <div class="logo">
+                    <h1>Dashboard</h1>
+                  </div>
+                  <p>Welcome to the CCL Information System</p>
+                </div>
+              </div>
+            </div>
+            <!-- Form Panel    -->
+            <div class="col-lg-6">
+              <div class="form d-flex align-items-center">
+                <div class="content">
+                  <form method="post" action="login.php" class="form-validate mb-4">
+                    <div class="form-group">
+                      <input id="login-username" type="text" name="username" required data-msg="Please enter your username" class="input-material">
+                      <label for="login-username" class="label-material">User Name</label>
+                    </div>
+                    <div class="form-group">
+                      <input id="login-password" type="password" name="password" required data-msg="Please enter your password" class="input-material">
+                      <label for="login-password" class="label-material">Password</label>
+                    </div>
+                    <button type="submit" name="submit"class="btn btn-primary">Login</button>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        
+        <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="js/front.js"></script>
+  </body>
+</html>
